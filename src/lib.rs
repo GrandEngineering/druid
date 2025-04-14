0a589ff65a2b9f63d58eae78b05fe0f3ad59ab25
IGN-Styly <claudioresendeptbr@gmail.com>
2025-04-15 00:50:52 +0100

    uuidv7

--- src/lib.rs
+++ src/lib.rs
@@ -33,3 +33,37 @@ impl Druid {
         hex_string
     }
 }
+
+pub struct DruidV7 {
+    pub id: [u8; 16],
+}
+impl Default for DruidV7 {
+    fn default() -> Self {
+        let mut bytes: [u8; 16] = [0u8; 16];
+        let timestamp: [u8; 16] = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_millis()
+            .to_be_bytes();
+        rand::rng().fill_bytes(&mut bytes);
+        bytes[0] = timestamp[10];
+        bytes[1] = timestamp[11];
+        bytes[2] = timestamp[12];
+        bytes[3] = timestamp[13];
+        bytes[4] = timestamp[14];
+        bytes[5] = timestamp[15];
+        bytes[6] = (bytes[6] & 0b00001111) | 0b01110000;
+        bytes[8] = (bytes[8] & 0b00111111) | 0b10000000;
+        Self { id: bytes }
+    }
+}
+impl DruidV7 {
+    pub fn to_hex(&self) -> String {
+        let bytes = self.id;
+        let hex_string: String = bytes
+            .iter()
+            .map(|byte| format!("{:02x}", byte)) // Format each byte as a 2-digit hex
+            .collect();
+        hex_string
+    }
+}
