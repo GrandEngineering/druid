be852ca74f6a8669089a57d8f8ba51d7c7134b48
IGN-Styly <claudioresendeptbr@gmail.com>
2025-05-05 20:28:00 +0100

    fix example

--- examples/id/src/main.rs
+++ examples/id/src/main.rs
@@ -3,18 +3,17 @@ use rand::{seq::SliceRandom, thread_rng};
 use std::hint::black_box;
 fn main() {
     let mut uids = Vec::new();
-    for _ in 0..10 {
-        uids.push(black_box(Druid::default().to_hex()));
+    for _ in 0..1000 {
+        uids.push(Druid::default().to_hex());
     }
     let org = uids.clone();
     let mut rng = thread_rng();
     uids.shuffle(&mut rng);
     uids.sort_by(|a, b| {
-        let timestamp_a = &a[0..16];
-        let timestamp_b = &b[0..16];
-        let ts_a = u64::from_str_radix(timestamp_a, 16).unwrap_or(0);
-        let ts_b = u64::from_str_radix(timestamp_b, 16).unwrap_or(0);
-        println!("{}", ts_a - ts_b);
+        let timestamp_a = &a[0..32];
+        let timestamp_b = &b[0..32];
+        let ts_a = u128::from_str_radix(timestamp_a, 32).unwrap_or(0);
+        let ts_b = u128::from_str_radix(timestamp_b, 32).unwrap_or(0);
         ts_a.cmp(&ts_b)
     });
     // println!("{:#?}", org);
