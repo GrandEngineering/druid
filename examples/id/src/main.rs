801365f097b6b437314d0deaa72fd85be6af0a8f
IGN-Styly <claudioresendeptbr@gmail.com>
2025-05-05 20:35:30 +0100

    fix example

--- examples/id/src/main.rs
+++ examples/id/src/main.rs
@@ -9,13 +9,14 @@ fn main() {
     let org = uids.clone();
     let mut rng = thread_rng();
     uids.shuffle(&mut rng);
-    uids.sort_by(|a, b| {
-        let timestamp_a = &a[0..32];
-        let timestamp_b = &b[0..32];
-        let ts_a = u128::from_str_radix(timestamp_a, 32).unwrap_or(0);
-        let ts_b = u128::from_str_radix(timestamp_b, 32).unwrap_or(0);
-        ts_a.cmp(&ts_b)
-    });
+    // uids.sort_by(|a, b| {
+    //     let timestamp_a = &a[0..32];
+    //     let timestamp_b = &b[0..32];
+    //     let ts_a = u128::from_str_radix(timestamp_a, 32).unwrap_or(0);
+    //     let ts_b = u128::from_str_radix(timestamp_b, 32).unwrap_or(0);
+    //     ts_a.cmp(&ts_b)
+    // });
+    uids.sort();
     // println!("{:#?}", org);
     // println!("{:#?}", uids);
     assert!(uids == org);
